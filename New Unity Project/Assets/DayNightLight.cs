6202f78d55178d0c3c669789dc726be2d28535f3
ShaddySora <jjmmff0@gmail.com>
2018-10-12 22:56:22 -0400

    Base Lighting

---
+++ New Unity Project/Assets/DayNightLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightLight : MonoBehaviour {
+
+    public float rotateSpeed = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.left, rotateSpeed*Time.deltaTime);
+	}
+}
