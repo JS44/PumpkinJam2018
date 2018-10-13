30a0d78906506649f6f2d6c279deb86bb4c67c59
JS44 <jacobsme@umich.edu>
2018-10-13 00:03:02 -0400

    trader start

---
+++ New Unity Project/Assets/Scripts/Traders.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Traders : MonoBehaviour {
+
+    FirstPersonController player;
+
+    public Text myText;
+    public float maxTradeDistance = 69f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GetComponent<FirstPersonController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseOver()
+    {
+        if (abs(player.transform.position - this.transform.postion) < maxTradeDistance)
+        {
+            myText.enabled = true;
+        }
+        else
+        {
+            myText.enabled = false;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        myText.enabled = false;
+    }
+}
