74cfcfbfc9290d99400ab938b5aba83a9680a44a
ShaddySora <jjmmff0@gmail.com>
2018-10-12 23:44:26 -0400

    Day/Night improvements

--- New Unity Project/Assets/DayNightLight.cs
+++ New Unity Project/Assets/DayNightLight.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DayNightLight : MonoBehaviour {
+public class DayNightLight : MonoBehaviour
+{
 
     public float rotateSpeed = 0.01f;
+    public float timer = 0;
+    public float nightToDayRatio = 1;
+    public bool shouldTimerIncrease;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Rotate(Vector3.left, rotateSpeed*Time.deltaTime);
-	}
+    private float nightMult;
+    private float dayMult;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0;
+        nightMult = nightToDayRatio;
+        dayMult = 1 / nightToDayRatio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentXRot = transform.rotation.eulerAngles.x;
+
+        if (currentXRot < 90 & currentXRot > 20)
+        {
+            transform.Rotate(Vector3.left, rotateSpeed * Time.deltaTime * nightMult);
+        }
+        else
+        {
+            transform.Rotate(Vector3.left, rotateSpeed * Time.deltaTime * dayMult);
+        }
+
+        if (shouldTimerIncrease)
+        {
+            timer = timer + Time.deltaTime;
+        }
+
+    }
+
+    public float GetGameTime()
+    {
+        return timer;
+    }
 }
